5ea72a4083c638131888eb1b9efb56ff48d29d0e
Гриша Васильев <gregory.vasiliev@gmail.com>
2024-03-22 18:24:14 +0300

    Добавьте файлы проекта.

---
+++ лаб 3 задание 2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+class Time {
+private:
+	int hour;
+	int minute;
+	int second;
+public:
+	void get() {
+		cin >> hour;
+		cin >> minute;
+		cin >> second;
+		cout << "Введенные данные: часы- " << hour<<" " << "минуты- " << minute << " " << "секунды- " << second <<" " << endl;
+	}
+	void function1() {
+		double res = (hour * 60) + minute + (second / 60);
+		if (hour < 24) {
+			int minutesUntilMidnight = (24 * 60) - res;
+			cout << "До полуночи осталось " << minutesUntilMidnight << " минут" << std::endl;
+		}
+		else {
+			cout << "Уже полночь" << std::endl;
+		}
+	}
+	void function2() {
+		int totalSeconds = hour * 3600 + minute * 60 + second;
+		totalSeconds += 100 * 60;
+		double hour1 = totalSeconds / 3600;
+		double minute1 = (totalSeconds % 3600) / 60;
+		double second1 = totalSeconds % 60;
+
+		cout << "Новое время: " << hour1 << " часов " << minute1 << " минут " << second1 << " секунд." << std::endl;
+	}
+	~Time() {
+		cout << "Объект Time был уничтожен" << endl;
+	}
+};
+	int main() {
+		setlocale(LC_ALL, "RU");
+		Time time;
+		time.get();
+		time.function1();
+		time.function2();
+	}
